c166bc360ec8e33cad809cdaced0fedaea742847
Ito Taichi <67674781+t4ichi@users.noreply.github.com>
2025-04-26 19:13:57 +0900

    initial commit

---
+++ frontend/.storybook/preview.ts
@@ -0,0 +1,33 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+import type { Preview } from "@storybook/react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/material-icons";
+import { darkTheme, lightTheme } from "../src/app/theme.ts";
+
+const preview: Preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+	decorators: [
+		withThemeFromJSXProvider({
+			GlobalStyles: CssBaseline,
+			Provider: ThemeProvider,
+			themes: {
+				light: lightTheme,
+				dark: darkTheme,
+			},
+			defaultTheme: "light",
+		}),
+	],
+};
+
+export default preview;
---
+++ frontend/src/app/theme.ts
@@ -0,0 +1,20 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+export const lightTheme = createTheme({
+	palette: {
+		mode: "light",
+	},
+	typography: {
+		fontFamily: "var(--font-roboto)",
+	},
+});
+
+export const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+	typography: {
+		fontFamily: "var(--font-roboto)",
+	},
+});
